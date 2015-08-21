7eb628123e91587d3bdc8c126c001b31b188d02c
RJ and Ariel <rj+ariel@gmail.com>
2015-08-21 10:58:16 -0400

    Create Sub and Post models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resource :session, only: [:new, :create, :destroy]
+  resources :subs do
+    resources :posts, only: [:index, :new, :create]
+  end
+  resources :posts, except: [:index, :new, :create]
 end
