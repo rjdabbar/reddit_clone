79239bcec15d25b018805b6146530e7c55239010
RJ and Ariel <rj+ariel@gmail.com>
2015-08-21 10:31:52 -0400

    Add #user_heading helper to application layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def log_in!(user)
+    session[:token] = user.reset_session_token!
+  end
+
+  def log_out!
+    current_user.reset_session_token!
+    session[:token] = nil
+  end
+
+  def current_user
+    User.find_by(session_token: session[:token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+  def ensure_logged_in
+    redirect_to new_user_url unless logged_in?
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
              value="#{form_authenticity_token}">
     HTML
   end
+
+  def user_heading
+    if logged_in?
+      delete_button(session_url, "Log out")
+    else
+      "<a href=\"#{new_session_url}\">Log in</a>".html_safe
+      "<a href=\"#{new_user_url}\">Register</a>".html_safe
+    end
+  end
+
+  def delete_button(action_url, button_text)
+    <<-HTML.html_safe
+    <form action="#{action_url}" method="post">
+      <input type="hidden" name="_method" value="delete">
+      <button>#{button_text}</button>
+    </form>
+    HTML
+  end
 end
